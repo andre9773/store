b1f9129bea9b07ae4c7ad2f76c7505f172be210b
a-aterytsia@microsoft.com <a-aterytsia@microsoft.com>
2023-10-27 11:49:11 +0100

    Created Product

---
+++ domain/Store/Product.cs
@@ -0,0 +1,36 @@
+﻿namespace Store;
+
+public class Product
+{   
+    public Guid Id { get; set; }
+
+    public string Title { get;} = string.Empty;
+
+    public string DunnceStoreLink { get; } = string.Empty;
+
+    public string AldiStoreLink { get; } = string.Empty;
+
+    public string LidlStoreLink { get; } = string.Empty;
+
+    public double DunnceStorePrice { get; }
+
+    public double AldiStorePrice { get; }
+
+    public double LidlStorePrice { get; }
+
+    public string Description { get; } = string.Empty;
+
+
+    public Product(Guid id, string title, string dunnceStoreLink, string aldiStoreLink, string lidlStoreLink, double dunnceStorePrice, double aldiStorePrice, double lidlStorePrice, string description)
+    {
+        Id = id;
+        Title = title;
+        DunnceStoreLink = dunnceStoreLink;
+        AldiStoreLink = aldiStoreLink;
+        LidlStoreLink = lidlStoreLink;
+        DunnceStorePrice = dunnceStorePrice;
+        AldiStorePrice = aldiStorePrice;
+        LidlStorePrice = lidlStorePrice;
+        Description = description;
+    }
+}
